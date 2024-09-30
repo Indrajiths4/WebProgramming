b0e26e9d2d23004afd02ba8f15314d0d7b6311f4
Indrajith S Nair <indrajithmundackal@gmail.com>
2024-09-30 09:50:16 +0530

    Update index.php

--- php/index.php
+++ php/index.php
@@ -6,47 +6,125 @@
     <title>PHP</title>
 </head>
 <body>
+    
     <?php 
-        $cars=array("BMW","Porsche","audi");
-        for($i=0;$i<count($cars);$i++) {
-            print($cars[$i]."<br>");
-        }
-        print("<br>");
-        foreach($cars as $x) {
-            print($x."<br>");
-        }
-        print("<br>");
-        $ages=array("Peter"=>32,"Brian"=>24);
-        foreach($ages as $ageperson=>$agevalue) {
-            print("$ageperson=>$agevalue<br>");
-        }
-        print("<br>");
-        $mon=array("January"=>"first","February"=>"second","March"=>"third","April"=>"fourth","May"=>"fifth","June"=>"sixth");
-        foreach($mon as $month=>$number) {
-            print("$month is the $number month.<br>");
-        }
-        print("<br>");
-        $fruits=array("Orange","Guava","Apple","Banana");
-        sort($fruits);
-        foreach($fruits as $fruit) {
-            print($fruit."<br>");
-        }
-        print("<br>");
-        rsort($fruits);
-        foreach($fruits as $fruit) {
-            print($fruit."<br>");
-        }
-        print("<br>");
-        ksort($mon);
-        foreach($mon as $month=>$number) {
-            print("$month is the $number month.<br>");
-        }
-        print("<br>");
-        asort($mon);
-        foreach($mon as $month=>$number) {
-            print("$month is the $number month.<br>");
-        }
-        print("<br>");
+        // $cars=array("BMW","Porsche","audi");
+        // for($i=0;$i<count($cars);$i++) {
+        //     print($cars[$i]."<br>");
+        // }
+        // print("<br>");
+        // foreach($cars as $x) {
+        //     print($x."<br>");
+        // }
+        // print("<br>");
+        // $ages=array("Peter"=>32,"Brian"=>24);
+        // foreach($ages as $ageperson=>$agevalue) {
+        //     print("$ageperson=>$agevalue<br>");
+        // }
+        // print("<br>");
+        // $mon=array("January"=>"first","February"=>"second","March"=>"third","April"=>"fourth","May"=>"fifth","June"=>"sixth");
+        // foreach($mon as $month=>$number) {
+        //     print("$month is the $number month.<br>");
+        // }
+        // print("<br>");
+        // $fruits=array("Orange","Guava","Apple","Banana","Mango");
+        // sort($fruits);
+        // foreach($fruits as $fruit) {
+        //     print($fruit."<br>");
+        // }
+        // print("<br>");
+        // rsort($fruits);
+        // foreach($fruits as $fruit) {
+        //     print($fruit."<br>");
+        // }
+        // print("<br>");
+        // ksort($mon);
+        // foreach($mon as $month=>$number) {
+        //     print("$month is the $number month.<br>");
+        // }
+        // print("<br>");
+        // asort($mon);
+        // foreach($mon as $month=>$number) {
+        //     print("$month is the $number month.<br>");
+        // }
+        // print("<br>");
+        // array_push($fruits,"Strawberry");
+        // foreach($fruits as $fruit) {
+        //     print($fruit."<br>");
+        // }
+        // print("<br>");
+        // $elementtoremove="Apple";
+        // $index=array_search($elementtoremove,$fruits);
+        // if($index!==false) {
+        //     unset($fruits[$index]);
+        // }
+        // foreach($fruits as $fruit) {
+        //     print($fruit."<br>");
+        // }
+        // var_dump($fruits);
+        // print("<br>");
+        // print("<br>");
+        // $array1=array('1','2','3','4','5');
+        // $array2=array('1','3');
+        // if(array_intersect($array2,$array1)===$array2) {
+        //     print("Subset.<br>");
+        // }
+        // else {
+        //     print("Not a subset.<br>"); 
+        // }
+        // print("<br>");
+        // print(strtoupper('hello world'));
+        // print("<br>");
+        // print("<br>");
+        // function helloworld() {
+        //     print("Hello World!<br>");
+        // }
+        // helloworld();
+        // print("<br>");
+        // print("<br>");
+        // function nameage($name,$year) {
+        //     print("$name born in $year.<br>");
+        // }
+        // nameage("John",2002);
+        // nameage("Manu",2004);
+        // print("<br>");
+        // function add(int $a,int $b) {
+        //     return $a+$b;
+        // }
+        // print(add(5,15.5)."<br>");
+        // print("<br>");
+        // //1
+        // $ages=array("Harry"=>21,"Alice"=>20,"Megha"=>22,"Bob"=>19);
+        // foreach($ages as $name=>$number) {
+        //     print($name."=>".$number."<br>");
+        // }
+        // print("<br>");
+        // //2
+        // $ages["Megha"]=28;
+        // foreach($ages as $name=>$number) {
+        //     print($name."=>".$number."<br>");
+        // }
+        // print("<br>");
+        // //3
+        // asort($ages);
+        // foreach($ages as $name=>$number) {
+        //     print($name."=>".$number."<br>");
+        // }
+        // print("<br>");
+        // //4
+        // print($ages["Alice"]);
+        // print("<br>");
+        // print("<br>");
+        // function add3(&$value) {
+        //     $value+=3;
+        // }
+        // $num2=50;
+        // add3($num2);
+        // print("Number : $num2");
+        // print("<br>");
+
+
+
     ?>
 </body>
 </html>
