01239033d8c2595a259dc1a86030b0c32a42871a
Indrajith S Nair <indrajithmundackal@gmail.com>
2024-10-19 09:56:44 +0530

    Add files via upload

---
+++ project/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="styles.css" rel="stylesheet"/>
+</head>
+<body>
+    <nav class="navlink">
+        <div class="heading"><img src="images/foodshare.png" alt=""><h2>Food sharing App</h2></div>
+        <div class="gotolinks">
+            <a href="placeorder.php">Place Order</a>
+            <a href="">Insert Order</a>
+            <a href="">Cart</a>
+        </div>
+</nav>
+</body>
+</html>
